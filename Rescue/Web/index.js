b066038fd45645ca66fbc4ab59d1f4e60dda5d0e
sunny8751 <sunny8751@gmail.com>
2017-10-27 22:54:08 -0400

    Switched to Express framework

--- Rescue/Web/index.js
+++ Rescue/Web/index.js
@@ -1,60 +1,35 @@
-const http = require('http');
-const fs = require('fs');
-const url = require('url');
-const firebase = require('firebase');
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+var app = express();
 
-// set up firebase
-var config = {
-  apiKey: "AIzaSyAZwcjPfmUrrurbK03aIX-7s7ydnP7dCuo",
-  authDomain: "project-380236083170.firebaseapp.com",
-  databaseURL: "https://rescue-cf4dc.firebaseio.com",
-  // storageBucket: "<BUCKET>.appspot.com",
-};
-firebase.initializeApp(config);
+// View Engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
-var database = firebase.database();
-var userId = null
+// Body Parser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
+// Set Static Path
+app.use(express.static(path.join(__dirname, 'static')));
 
-// var userId = firebase.auth().currentUser.uid;
-// return firebase.database().ref('/Users/' + userId).once('value').then(function(snapshot) {
-//   var username = (snapshot.val() && snapshot.val().username) || 'Anonymous';
-//   // ...
-// });
-
-const server = http.createServer((req, res) => {
-	var q = url.parse(req.url, true);
-    var filename = "." + q.pathname;
-    fs.readFile(filename, function(err, data) {
-      if (err) {
-        res.writeHead(404, {'Content-Type': 'text/html'});
-        return res.end("404 Not Found");
-      }  
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write(data);
-      return res.end();
-    });
+app.get('/', function(req, res) {
+	// res.send('Hello World');
+	res.render('index', {
+		title: 'Login'
+	});
 });
 
-server.listen(port, hostname, () => {
-	console.log('Server started on port ' + port);
-});
+app.post('/login', function(req, res) {
+	var newUser = {
+		email: req.body.email,
+		password: req.body.password
+	}
+	console.log(newUser);
+})
 
-// fs.readFile('index.html', (err, html) => {
-// 	if (err) {
-// 		throw err;
-// 	}
-// 	const server = http.createServer((req, res) => {
-// 		res.statusCode = 200;
-// 		res.setHeader('Content-type', 'text/html');
-// 		res.write(html);
-// 		res.end();
-// 	});
-
-// 	server.listen(port, hostname, () => {
-// 		console.log('Server started on port ' + port);
-// 	});
-// });
+app.listen(3000, function() {
+	console.log('Server started on port 3000');
+})
