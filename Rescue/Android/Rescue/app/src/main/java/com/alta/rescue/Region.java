0b755f720cc2f510d3be5ebb1eb7b2d8ff491c05
David Margolin <trickedoutdavid@gmail.com>
2017-10-27 23:39:27 -0400

    progress?

---
+++ Rescue/Android/Rescue/app/src/main/java/com/alta/rescue/Region.java
@@ -0,0 +1,19 @@
+package com.alta.rescue;
+
+import java.util.ArrayList;
+
+/**
+ * Created by David Margolin on 10/27/2017.
+ */
+
+public class Region {
+    public Region(String name, ArrayList<Briefing> briefs, ArrayList<String> users) {
+        this.name = name;
+        this.briefs = briefs;
+        this.users = users;
+    }
+
+    String name;
+    ArrayList<Briefing> briefs;
+    ArrayList<String> users;
+}
