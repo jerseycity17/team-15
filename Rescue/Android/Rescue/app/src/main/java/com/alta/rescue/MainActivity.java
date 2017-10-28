6994278e3984d7c42d4735649ae27bf58b72f164
David Margolin <trickedoutdavid@gmail.com>
2017-10-27 20:16:48 -0400

    integrated analytics

--- Rescue/Android/Rescue/app/src/main/java/com/alta/rescue/MainActivity.java
+++ Rescue/Android/Rescue/app/src/main/java/com/alta/rescue/MainActivity.java
@@ -7,10 +7,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import com.google.firebase.analytics.FirebaseAnalytics;
+
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
-
+    private FirebaseAnalytics mFirebaseAnalytics;
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
@@ -36,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
