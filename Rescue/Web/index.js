bd34fc8ded556b3cef6645a254708723c58d771d
sunny8751 <sunny8751@gmail.com>
2017-10-27 23:52:32 -0400

    Added login and register  users page

--- Rescue/Web/index.js
+++ Rescue/Web/index.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
+const firebase = require('firebase');
 
 var app = express();
 
 // View Engine
-app.set('view engine', 'ejs');
+app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 // Body Parser Middleware
@@ -15,6 +16,24 @@ app.use(bodyParser.urlencoded({extended: false}));
 // Set Static Path
 app.use(express.static(path.join(__dirname, 'static')));
 
+// set up firebase
+var config = {
+  apiKey: "AIzaSyAZwcjPfmUrrurbK03aIX-7s7ydnP7dCuo",
+  authDomain: "project-380236083170.firebaseapp.com",
+  databaseURL: "https://rescue-cf4dc.firebaseio.com",
+  // storageBucket: "<BUCKET>.appspot.com",
+};
+firebase.initializeApp(config);
+
+// var database = firebase.database();
+// var userId = null
+
+// var userId = firebase.auth().currentUser.uid;
+// return firebase.database().ref('/Users/' + userId).once('value').then(function(snapshot) {
+//   var username = (snapshot.val() && snapshot.val().username) || 'Anonymous';
+//   // ...
+// });
+
 app.get('/', function(req, res) {
 	// res.send('Hello World');
 	res.render('index', {
@@ -23,11 +42,34 @@ app.get('/', function(req, res) {
 });
 
 app.post('/login', function(req, res) {
-	var newUser = {
-		email: req.body.email,
-		password: req.body.password
-	}
-	console.log(newUser);
+	var email = req.body.email;
+	var password = req.body.password;
+	firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+	    // Handle Errors here.
+	    var errorCode = error.code;
+	    var errorMessage = error.message;
+	});
+	res.render('briefings', {title: 'Briefings'});
+	console.log("Logged in as " + email);
+})
+
+app.post('/register', function(req, res) {
+	var email = req.body.email;
+	var password = req.body.password;
+	// login to firebase
+	firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+		// Handle Errors here.
+		var errorCode = error.code;
+		var errorMessage = error.message;
+		return;
+	});
+	var first = req.body.first;
+	var last = req.body.last;
+	var phone = req.body.phone;
+	
+	// add user information to database
+	res.render('briefings', {title: 'Briefings'});
+	console.log("Created new user with email " + email);
 })
 
 app.listen(3000, function() {
